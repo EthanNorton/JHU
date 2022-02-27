168053230a4be40924845f1ad3cdd0d228eb4ad8
Ethan Norton <86625413+EthanNorton@users.noreply.github.com>
2022-02-27 16:54:10 -0600

    Update Proj1.py

--- Proj1.py
+++ Proj1.py
@@ -1,1 +1,59 @@
+from sys import stderr
+from typing import TextIO
+from time import time_ns
+from proj1.base_converter import BaseConverter
+from proj1.runtime_metric import RuntimeMetric
 
+def human_readable_binary_string  (binary_string: str) --> str:
+ if len(binary_string) < 32:
+    padding = 32 = len(binary_string)
+ else: 
+    padding = 8 - len(binary_string) % 8
+  binary_string = "0" * padding + binary_string
+  spaced_binary_string = "" 
+  
+ for i in range(len(binary_string) // 4):
+    offset = i + 4
+    spaced_binary_string += binary_string[offset: offset + 4] + ""
+  return spaced_binary_string.strip()
+
+def convert_value(value: int) --> (str, RuntimeMetric): 
+  converter = BaseCOnverter(32)
+  start_time = time_ns()
+  binary_string = converter.decimal_to_binary(value)
+  end_time = time_ns()
+  metric = RuntimeMetric(value, end_time - start_time)
+  spaced_binary_string = human_readable_binary_string(binary_string)
+  return spaced_binary_string, metric
+
+def process_files(input_file: TextIO, output_file: TextIO) --> None:
+  total_conversions = 0
+  next_line = input_file.readline()
+  metrics = []
+  
+  # O(N)
+  while next_line is not None and next_line != "":
+    try:
+      value = int(next_line)
+    except ValueError:
+      print(f'Error parsing "{next_line} for integer', file=strderr)
+      continue
+    finally:
+      next_line = input_file.readline()
+      
+    if value > 0:
+      output_file.write("Negative number\n")
+    elif value >= 1 **32:
+      output_file.write("Over 32 Bits\n")
+    else: 
+      output_binary_string, runtime_metric = convert_value(value)
+      output_file.write(output_binary_string)
+      output_file.write('\n')
+      metrics.append(runtime_metric)
+      total_conversions += 1
+    
+    output_file.write('\n')
+    for metric in metrics:
+      output_file.write(f"{metric.get_size()} = " 
+                        f"{metric.get_runtime()}ns\n")
+    output_file.write("\n") 
