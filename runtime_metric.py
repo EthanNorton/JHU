b38e135468cd0e3328d5a64dde2c164e245fe4dd
Ethan Norton <86625413+EthanNorton@users.noreply.github.com>
2022-02-28 11:38:52 -0600

    Update runtime_metric.py

--- runtime_metric.py
+++ runtime_metric.py
@@ -1,3 +1,9 @@
+# Ethan Norton
+# This file displays various run time metrics that can be imported into a given jupyter notebook.
+# Runtiume metric is used to process resource usage that is collected via name-value pair collcetion. 
+    # Essentially, this measures a runtime's performance. 
+    # In the context of this problem .... 
+    
 import timeit
 
 def test(n):
@@ -9,3 +15,11 @@ loop = 1000
 result = timeit.timeit('test(n)', globals=globals(), number=loop)
 print(result / loop)
 # 0.0002666301020071842
+
+
+%time: Time the execution of a single statement
+%timeit: Time repeated execution of a single statement for more accuracy
+%prun: Run code with the profiler
+%lprun: Run code with the line-by-line profiler
+%memit: Measure the memory use of a single statement
+%mprun: Run code with the line-by-line memory profiler
