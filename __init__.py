5a60e28dcc23635c5c1d45be8195cd8e52b02b50
Ethan Norton <86625413+EthanNorton@users.noreply.github.com>
2022-02-28 15:50:29 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,23 @@
+# Ethan Norton
+# This file converts the stacks from stacks that are now appended (prefix mode) into postfix mode.
+  ## pop removes an item from the stack, and will be utilized to convert prefix to postfix.
+  ## push adds one or more elements to the end of an array. 
+
+# Importing different stacks
+from StackOfIntegers import stack1
+from StackOfIntegers import stack2
+from StackOfIntegers import stack3
+from StackOfIntegers import stack4
+from StackOfIntegers import stack5
+from StackOfIntegers import stack6
+from StackOfIntegers import stack7
+from StackOfIntegers import stack8
+from StackOfIntegers import stack9
+from StackOfIntegers import stack10
+from StackOfIntegers import stack11
+from StackOfIntegers import stack12
+from StackOfIntegers import stack13
+
+# Converting stack1 to postfix 
+
 
