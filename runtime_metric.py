91d6a0b8734d7126ccbfc1baa9302afe4f05852f
Ethan Norton <86625413+EthanNorton@users.noreply.github.com>
2022-02-28 11:27:44 -0600

    Update runtime_metric.py

--- runtime_metric.py
+++ runtime_metric.py
@@ -1,1 +1,11 @@
+import timeit
 
+def test(n):
+    return sum(range(n))
+
+n = 10000
+loop = 1000
+
+result = timeit.timeit('test(n)', globals=globals(), number=loop)
+print(result / loop)
+# 0.0002666301020071842
