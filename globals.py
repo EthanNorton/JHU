23561fb83dedf688ce7c5a0aacbb9e87268ea80f
Ethan Norton <86625413+EthanNorton@users.noreply.github.com>
2022-02-28 22:57:39 -0600

    Update globals.py

--- globals.py
+++ globals.py
@@ -1,4 +1,5 @@
-def initialize(): 
-    global num 
-    num = 1 
+def init(): 
+    global myList
+    myList = [stack1, stack2, stack3, stack4, stack5, stack6, stack7, stack8, stack9, stack10, stack11, stack12, stack13]
+    
     
