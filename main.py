116bbafef904c80b5129a83b0f31b18ae565c002
Ethan Norton <86625413+EthanNorton@users.noreply.github.com>
2022-02-28 23:29:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 ### Ethan Norton
 # Input and Read the Characters line by line.
 # Main = LAUNCH the program
-# This is the compiled code 
+# This file connects all code, and is best for connecting the other source code found throughout the document.
+# This file combines all of the source code, and produces the output that is found in the output file.
+  # This attempts to efficiently convert all prefix to postfix, through the stacks data structure. 
 
 import globals
 from _init__ import globals
