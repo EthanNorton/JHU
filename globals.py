617f0547ab1639a88748c9c59b9adf7c53aba352
Ethan Norton <86625413+EthanNorton@users.noreply.github.com>
2022-02-28 23:18:24 -0600

    Update globals.py

--- globals.py
+++ globals.py
@@ -1,3 +1,6 @@
+### Ethan Norton
+# This file creates globals that can be accessed across all files in this folder.
+# The variables globalized will be the various stacks that will be the central focus of this project.
 def init(): 
     global myList
     myList = [stack1, stack2, stack3, stack4, stack5, stack6, stack7, stack8, stack9, stack10, stack11, stack12, stack13]
