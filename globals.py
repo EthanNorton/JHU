daa9c90d06f88d8a30c7dc074b440ff605130626
Ethan Norton <86625413+EthanNorton@users.noreply.github.com>
2022-08-11 13:46:01 -0500

    Delete globals.py

--- globals.py
+++
@@ -1,9 +0,0 @@
-### Ethan Norton
-# This file creates globals that can be accessed across all files in this folder.
-# The variables globalized will be the various stacks that will be the central focus of this project.
-# The variables will have an interconnectedness across all files!
-def init(): 
-    global myList
-    myList = [stack1, stack2, stack3, stack4, stack5, stack6, stack7, stack8, stack9, stack10, stack11, stack12, stack13]
-    
-    
